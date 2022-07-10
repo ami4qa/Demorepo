38b1f343af9cff8264ec7ae21991ad2cf4a15917
ami4qa <ami4qa@gmail.com>
2022-07-09 21:38:10 -0400

    added austration

---
+++ src/javaoperator/substartion.java
@@ -0,0 +1,16 @@
+package javaoperator;
+
+public class substartion {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=5;
+		int c=a-b;
+		
+		System.out.println("substation of two numbers:"+c);
+		
+				
+
+	}//eoc
+
+}//eom
