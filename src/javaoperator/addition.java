1ef03cebd3911dd0c8b164cde9f72e6f5ede69f2
ami4qa <ami4qa@gmail.com>
2022-07-09 21:05:29 -0400

    first git commit for my java

---
+++ src/javaoperator/addition.java
@@ -0,0 +1,17 @@
+package javaoperator;
+
+public class addition {
+
+public static void main(String args[]) {
+	
+		int a =10;
+		int b=20;
+		int c=a+b;
+		System.out.println("Sum of a+b="+c);
+
+		}//eoc
+
+	}//eom
+
+
+
